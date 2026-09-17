377be5f57173957f9ad052429e57e474a07dff02
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-09-17 20:32:06 +0545

    scsi: sd: Demote cache synchronization message to debug in sd_shutdown

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3552,7 +3552,7 @@ static void sd_shutdown(struct device *dev)
 		return;
 
 	if (sdkp->WCE && sdkp->media_present) {
-		sd_printk(KERN_NOTICE, sdkp, "Synchronizing SCSI cache\n");
+		sd_printk(KERN_DEBUG, sdkp, "Synchronizing SCSI cache\n");
 		sd_sync_cache(sdkp, NULL);
 	}
 
