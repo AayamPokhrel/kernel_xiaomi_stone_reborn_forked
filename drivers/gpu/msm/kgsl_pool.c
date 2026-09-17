12317561b6f3a36ee889452a7847cc097b76d878
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-09-17 17:02:49 +0545

    msm: kgsl: Fix pool page_count atomic read in tracepoint

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -105,7 +105,7 @@ static void _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 	llist_add((struct llist_node *)&p->lru, &pool->page_list);
 	atomic_inc(&pool->page_count);
 
-	trace_kgsl_pool_add_page(pool->pool_order, pool->page_count);
+	trace_kgsl_pool_add_page(pool->pool_order, atomic_read(&pool->page_count));
 	mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
 			    (1 << pool->pool_order));
 }
